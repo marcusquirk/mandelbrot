818406e310b1c065fd1fe0a09fdd9c8f19623b4c
marcusquirk <marcus@192.168.0.19>
2020-04-14 08:55:11 -0500

    Repurposed SimpleBitmap to draw parts of the mandelbrot set.
    It draws the part of the set for (-1.3, -1.1, -0.4, -0.2) in yellow and
    black.

--- Mandelbrot/src/mandelbrot/SimpleBitmap.java
+++ Mandelbrot/src/mandelbrot/SimpleBitmap.java
@@ -2,25 +2,20 @@ package mandelbrot;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.util.Random;
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
-class SimpleBitmap extends JPanel implements ActionListener {
+class SimpleBitmap extends JPanel {
 
-    private static final int WIDTH = 512;
-    private static final int HEIGHT = 512;
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 700;
     private BufferedImage image;
     private Random rng = new Random();
 
     public SimpleBitmap() {
-        Timer timer = new Timer(300, this);
-        timer.start();
         
         int w = WIDTH;
         int h = HEIGHT;
@@ -44,22 +39,20 @@ class SimpleBitmap extends JPanel implements ActionListener {
         int[] random = {rng.nextInt(255), rng.nextInt(255), rng.nextInt(255)};
         int[] random2 = {rng.nextInt(255), rng.nextInt(255), rng.nextInt(255)};
 
+        Mandelbrot mandelbrot = new Mandelbrot(-1.3, -1.1, -0.4, -0.2);
+        
         for (int j = 0; j < h; j++) {
             for (int i = 0; i < w; i++) {
-                if (Math.abs(i-j) < 10) {
-                    raster.setPixel(i, j, yellow);
-                } // if
-                else if ((int)((double) i / j) == 2){
-                    raster.setPixel(i, j, random);
-                }
-                else if (rng.nextInt(8)<rng.nextInt(4)){
-                    raster.setPixel(i, j, random2);
-                }
-                else if (Math.abs((w-i)-(30+j)) < 5) {
-                    raster.setPixel(i, j, random);
-                } // if
-                else {
-                    raster.setPixel(i, j, black);
+                double mandelbrotValue = mandelbrot.check(i, j, 1, w, 1, h);
+ //               System.out.println(mandelbrotValue);
+                raster.setPixel(i, j, yellow);
+                if (mandelbrotValue < 10){
+                    raster.setPixel(i,j,black);
+//                } else if (mandelbrotValue < 25 && mandelbrotValue>20){
+//                    raster.setPixel(i,j, random);
+                } else if (mandelbrotValue < 35){
+                    raster.setPixel(i,j, new int[]{0,10*(int)(mandelbrotValue-10),
+                        5*(int)(mandelbrotValue-10)});
                 }
             } // for
         } // for
@@ -67,10 +60,5 @@ class SimpleBitmap extends JPanel implements ActionListener {
         g2d.drawImage(image, scale, this);
         
     } // paintComponent( Graphics )
-    
-    @Override
-    public void actionPerformed(ActionEvent e){
-        this.repaint();
-    }
     
 } // simpleBitmap
