c16819d07575cada5d43617b0c8681393510e561
marcusquirk <marcus@192.168.0.19>
2020-04-14 12:24:22 -0500

    Added a timer to animate Julia sets.

--- Mandelbrot/src/mandelbrot/SimpleBitmap.java
+++ Mandelbrot/src/mandelbrot/SimpleBitmap.java
@@ -2,20 +2,29 @@ package mandelbrot;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.util.Random;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-class SimpleBitmap extends JPanel {
+class SimpleBitmap extends JPanel implements ActionListener {
 
     private static final int WIDTH = 700;
     private static final int HEIGHT = 700;
     private BufferedImage image;
     private Random rng = new Random();
+    private static double c1 = -2.0;
+    private static double c2 = -0.0;
+    private static boolean c1Up = true;
+    private static boolean c2Up = true;
 
     public SimpleBitmap() {
+        Timer timer = new Timer(30, this);
+        timer.start();
         
         int w = WIDTH;
         int h = HEIGHT;
@@ -39,11 +48,11 @@ class SimpleBitmap extends JPanel {
         int[] random = {rng.nextInt(255), rng.nextInt(255), rng.nextInt(255)};
         int[] random2 = {rng.nextInt(255), rng.nextInt(255), rng.nextInt(255)};
 
-        Mandelbrot mandelbrot = new Mandelbrot(-1.3, -1.1, -0.4, -0.2);
+        Mandelbrot mandelbrot = new Mandelbrot(-2, 1, -1.5, 1.5);
         
         for (int j = 0; j < h; j++) {
             for (int i = 0; i < w; i++) {
-                double mandelbrotValue = mandelbrot.check(i, j, 1, w, 1, h);
+                double mandelbrotValue = mandelbrot.check(i, j, 1, w, 1, h,c1,c2);
  //               System.out.println(mandelbrotValue);
                 raster.setPixel(i, j, yellow);
                 if (mandelbrotValue < 10){
@@ -60,5 +69,19 @@ class SimpleBitmap extends JPanel {
         g2d.drawImage(image, scale, this);
         
     } // paintComponent( Graphics )
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (c1<-1){c1Up = true;}
+        else if (c1>1){c1Up = false;}
+        if (c2<-1){c2Up = true;}
+        else if (c2>1){c2Up = false;}
+        
+        if (c1Up){c1 = c1+0.01;}
+        else {c1 = c1-0.01;}
+        if (c2Up){c2 = c2+0.03;}
+        else {c2 = c2-0.03;}
+        this.repaint();
+    }
     
 } // simpleBitmap
