00ea4de97ec201c014d51067dbd07d45012f6b88
marcusquirk <marcus@192.168.0.19>
2020-04-14 08:57:25 -0500

    Calculates if a pixel is in the Mandelbrot set.

--- Mandelbrot/src/mandelbrot/Mandelbrot.java
+++ Mandelbrot/src/mandelbrot/Mandelbrot.java
@@ -1,35 +1,53 @@
 package mandelbrot;
 
-import java.awt.Container;
-import javax.swing.JFrame;
+import java.awt.Color;
 
 /**
+ * A class that determines if a point is likely to be in the Mandelbrot set.
  *
  * @author marcus
  */
-public class Mandelbrot extends JFrame{
+public class Mandelbrot {
 
-    private static final int WIDTH = 512;
-    private static final int HEIGHT = 512;
-    private static final String TITLE = "Mandelbrot";
-    
-    public Mandelbrot() {
-        
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(WIDTH, HEIGHT);
-        this.setTitle("Mandelbrot");
-        
-        Container container = this.getContentPane();
-        
-        SimpleBitmap panel = new SimpleBitmap();
-        container.add(panel);
-        
-        this.setVisible(true);
+    private Complex c;
+    private double uMin;
+    private double uMax;
+    private double vMin;
+    private double vMax;
+
+    public Mandelbrot(double uMin, double uMax, double vMin, double vMax) {
+        this.uMin = uMin;
+        this.uMax = uMax;
+        this.vMin = vMin;
+        this.vMax = vMax;
     } // Mandelbrot()
-    
-    
+
+    public int check(int x, int y, int xMin, int xMax, int yMin, int yMax) {
+        c = this.mapping(x, y, xMin, xMax, yMin, yMax);
+        Complex z = new Complex(0, 0);
+        int loopCounter = 0;
+        Boolean continueLooping = true;
+        while (continueLooping) {
+            z = z.multiply(z);
+            z = z.add(c);
+            loopCounter++;
+            if (loopCounter == 100 | z.magnitude() > 2) {
+                continueLooping = false;
+            } // if
+        } // while
+        //    System.out.println(c + " " + loopCounter + " " + z.magnitude());
+        return loopCounter;
+    } // check( int, int )
+
+    public Complex mapping(int x, int y, int xMin, int xMax, int yMin, int yMax) {
+        //    System.out.println(x + ", " + y);
+        double u = (double) uMin + ((double) (uMax - uMin) * ((double) (x - xMin) / (xMax - xMin)));
+        double v = (double) vMin + ((double) (vMax - vMin) * ((double) (y - yMin) / (yMax - yMin)));
+        return new Complex(u, v);
+    }
+
     public static void main(String[] args) {
-        Mandelbrot mandelbrot = new Mandelbrot();
+        MandelbrotFrame mandelbrotFrame = new MandelbrotFrame();
+
     } // main()
-    
-} // Mandelbrot
+}
